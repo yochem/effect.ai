59da66894efa4379787bbee0853c0219929e92c2
Yochem van Rosmalen <yochem+git@icloud.com>
2020-06-09 16:58:55 +0200

    Use single quotes instead of doubles

--- split_length.py
+++ split_length.py
@@ -1,13 +1,5 @@
-"""
-Splits the captiongroup according to the 42 character limit.
-You can change sen to a captiongroup from the data.
-"""
-
-
 def check(data):
-    """
-    Checks if the current data is equal or less than 42 characters.
-    """
+    """Checks if the current data is equal or less than 42 characters."""
     return sum([len(x) for x in data]) <= 42
 
 
@@ -21,15 +13,10 @@ def split_length(data, number=42, splits=[]):
         splits.append(data)
         return splits
 
-    sentence = " ".join(data)
+    sentence = ' '.join(data)
     while sentence[number] != ' ':
         number -= 1
 
     splits.append(sentence[:number].split())
     split_length(sentence[number:].split(), splits=splits)
     return splits
-
-
-if __name__ == '__main__':
-    sen = "dit is een hele lange zin met echt wel meerdere characters".split()
-    split_length(sen)
