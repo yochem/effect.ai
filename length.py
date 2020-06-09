a0f706e7803b0f01af3260a5fe1dcbf7247d0289
Anne Kaal <anne.kaal12@gmail.com>
2020-06-09 15:58:02 +0200

    character split

--- length.py
+++ length.py
@@ -1,32 +1,35 @@
 """
 Splits the captiongroup according to the 42 character limit.
-def check checks if the current data is equal or less than 42 characters
-def splitlength splits the data accordingly and finds the first space adn makes
-the cut there.
-It does recursivly the same thing for words that are left in the sentence.
-
-You can change sen to a captiongroup from the data
+You can change sen to a captiongroup from the data.
 """
 
 
 def check(data):
+    """
+    Checks if the current data is equal or less than 42 characters.
+    """
     return sum([len(x) for x in data]) <= 42
 
 
-def splitlength(data, number=42, splits=[]):
+def split_length(data, number=42, splits=[]):
+    """
+    Splits the data accordingly and finds the first space and makes the cut
+    there. It does the same thing recursivly for words that are left in the
+    sentence.
+    """
     if check(data):
         splits.append(data)
         return splits
 
     sentence = " ".join(data)
-    while sentence[number] != " ":
+    while sentence[number] != ' ':
         number -= 1
 
     splits.append(sentence[:number].split())
-    splitlength(sentence[number:].split(), splits=splits)
+    split_length(sentence[number:].split(), splits=splits)
     return splits
 
 
 if __name__ == '__main__':
     sen = "dit is een hele lange zin met echt wel meerdere characters".split()
-    splitlength(sen)
+    split_length(sen)
