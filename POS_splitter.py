4300e66dabd8907565bdb65e332e42c009210ddf
lysa-n <lysa_ng@hotmail.com>
2020-06-10 14:12:25 +0200

    added POS-tag functions

--- POS_splitter.py
+++ POS_splitter.py
@@ -1,113 +1,105 @@
 from asr import ASR
-from pos_tagger import basic_pos, no_apos, basic_word_pos, Pos
+from pos_tagger import basic_pos, no_apos, basic_words_pos, Pos
 
-def pos_splitter_pron_verb(tagged_data, split_weight):
+def pos_splitter_pron_verb(words, split_weight=0.2):
     """
-    Adjust weight of word where split is not recommended.
+    Adjust weight of word where split is not recommended:
+        Avoid splitting between pronoun + verb
 
     see: https://bbc.github.io/subtitle-guidelines/#Break-at-natural-points
 
     Input:
-    1D list of Pos objects.
+    1D list of Word objects.
     Split-weight indicating the importance of not-splitting on the word.
 
-    Output: 1D list with Pos object with adjusted weights.
+    Output: 1D list of Word objects with adjusted weights.
     """
-    for index, word in enumerate(tagged_data[:-1]):
-        next_word = tagged_data[index+1]
+    tagged_words = basic_words_pos(words)
+
+    for index, word in enumerate(tagged_words[:-1]):
+        next_word = tagged_words[index+1]
 
         if word.tag == 'PRON' and next_word.tag == 'VERB':
-            word.weight = word.weight - split_weight
+            words[index].weight = words[index].weight - split_weight
 
+    return words
 
+def pos_splitter_det_noun(words, split_weight=0.3):
+    """
+    Adjust weight of word where split is not recommended:
+        Avoid splitting between determiner + noun
 
-def pos_splitter_det_noun(tagged_data, split_weight):
-    for index, word in enumerate(tagged_data[:-1]):
-        next_word = tagged_data[index+1]
+    see: https://bbc.github.io/subtitle-guidelines/#Break-at-natural-points
+
+    Input:
+    1D list of Word objects.
+    Split-weight indicating the importance of not-splitting on the word.
+
+    Output: 1D list of Word objects with adjusted weights.
+    """
+    tagged_words = basic_words_pos(words)
+
+    for index, word in enumerate(tagged_words[:-1]):
+        next_word = tagged_words[index+1]
 
         # Determiner and Noun
         if word.tag == 'DET' and next_word.tag == 'NOUN':
-            word.weight = word.weight - split_weight
+            words[index].weight = words[index].weight - split_weight
 
-    return tagged_data
+    return words
 
 
-def pos_splitter_prep_phrase(tagged_data, split_weight):
-    for index, word in enumerate(tagged_data[:-1]):
-        next_word = tagged_data[index+1]
-        nextnext = tagged_data[index+2]
-        nextnextnext = tagged_data[index+3]
+def pos_splitter_prep_phrase(words, split_weight=0.4):
+    """
+    Adjust weight of word where split is not recommended:
+        Avoid splitting between Preposition + following phrase
+
+    see: https://bbc.github.io/subtitle-guidelines/#Break-at-natural-points
+
+    Input:
+    1D list of Word objects.
+    Split-weight indicating the importance of not-splitting on the word.
+
+    Output: 1D list of Word objects with adjusted weights.
+    """
+    tagged_words = basic_words_pos(words)
+
+    for index, word in enumerate(tagged_words[:-1]):
+        next_word = tagged_words[index+1]
+        nextnext = tagged_words[index+2]
+        nextnextnext = tagged_words[index+3]
 
     	# Preposition and phrase
         if word.tag == 'ADP' and ((next_word.tag == 'DET' and nextnext.tag == 'NOUN') or (next_word.tag == 'ADJ' and nextnext.tag == 'NOUN') or (next_word.tag == 'DET' and nextnext.tag == 'ADJ' and nextnextnext.tag == 'NOUN') or (next_word.tag == 'PRON' and nextnext.tag == 'NOUN')):
-            word.weight = 0.4
+            words[index].weight = words[index].weight - split_weight
 
-    return tagged_data
+    return words
 
-def pos_splitter_conj_phrase(tagged_data, split_weight):
-    for index, word in enumerate(tagged_data[:-1]):
-        next_word = tagged_data[index+1]
-        nextnext = tagged_data[index+2]
-        nextnextnext = tagged_data[index+3]
+def pos_splitter_conj_phrase(tagged_words, split_weight=0.3):
+    """
+    Adjust weight of word where split is not recommended:
+        Avoid splitting between conjunction + following phrase
+
+    see: https://bbc.github.io/subtitle-guidelines/#Break-at-natural-points
+
+    Input:
+    1D list of Word objects.
+    Split-weight indicating the importance of not-splitting on the word.
+
+    Output: 1D list of Word objects with adjusted weights.
+    """
+    for index, word in enumerate(tagged_words[:-1]):
+        next_word = tagged_words[index+1]
+        nextnext = tagged_words[index+2]
+        nextnextnext = tagged_words[index+3]
 
         # Conjuction and phrase
-        if word.tag == 'CONJ' and ((next_word.tag == 'DET' and nextnext.tag == 'NOUN') or (next_word.tag == 'ADJ' and nextnext.tag == 'NOUN') or (next_word.tag == 'DET' and nextnext.tag == 'ADJ' and nextnextnext.tag == 'NOUN') or (next_word.tag == 'PRON' and nextnext.tag == 'NOUN')):
-			word.weight = 0.3
+        if word.tag == 'CONJ' and ((next_word.tag == 'DET' and nextnext.tag == 'NOUN') or
+        (next_word.tag == 'ADJ' and nextnext.tag == 'NOUN') or (next_word.tag == 'DET' and
+        nextnext.tag == 'ADJ' and nextnextnext.tag == 'NOUN') or
+        (next_word.tag == 'PRON' and nextnext.tag == 'NOUN')):
+            words[index].weight = words[index].weight - split_weight
 
-    return tagged_data
-
-
-def pos_splitter(tagged_data):
-    """
-
-    Merges word groups that cannot be split to one Word.
-        tag them with str 'No split'
-
-    Input: 1D list of Words.
-
-    Output: 1D list with words merged that cannot be split.
-    """
-
-    for index, word in enumerate(tagged_data):
-
-        next = tagged_data[index+1]
-        nextnext = tagged_data[index+2]
-        nextnextnext = tagged_data[index+3]
-
-		# Determiner plus noun
-        if word.tag == 'DT' and next.tag == 'NN':
-
-            tagged_data[index].tag = 'No split'
-            tagged_data[index].text = word.text + " " + next.text
-            tagged_data[index].end = next.end
-
-            tagged_data.remove(next)
-
-		# Preposition and phrase
-        if word.tag == 'ADP' and ((next.tag == 'DET' and nextnext.tag == 'NOUN') or (next.tag == 'ADJ' and nextnext.tag == 'NOUN') or (next.tag == 'DET' and nextnext.tag == 'ADJ' and nextnextnext.tag == 'NOUN') or (next.tag == 'PRON' and nextnext.tag == 'NOUN')):
-            word.weight = 0.4
-
-		# Conjuction and phrase
-        if word.tag == 'CONJ' and ((next.tag == 'DET' and nextnext.tag == 'NOUN') or (next.tag == 'ADJ' and nextnext.tag == 'NOUN') or (next.tag == 'DET' and nextnext.tag == 'ADJ' and nextnextnext.tag == 'NOUN') or (next.tag == 'PRON' and nextnext.tag == 'NOUN')):
-			word.weight = 0.3
-
-
-
-    return tagged_data
-
+    return words
 
 data = ASR('asr/sample01.asrOutput.json').groups()
-
-"""Create new list of words with POS-tags"""
-tagged_words = []
-
-for i in range(len(data)):
-    tagged_words.append(basic_word_pos(data[i]))
-
-# Voorbeeld
-lijst = [Pos(text='a', start='902.52', end='902.71', tag='DT'),
-Pos(text='very', start='902.72', end='903.29', tag='NN'),
-Pos(text='viable', start='903.46', end='904.04', tag='DT'),
-Pos(text='gaming',start='904.04', end='904.35', tag='NN')]
-
-print(pos_splitter(lijst))
--- pos_tagger.py
+++ pos_tagger.py
@@ -13,28 +13,33 @@ Creates new dataclass for words with start time, end time and POS tag.
 class Pos(Word):
     tag: str
 
-
-def basic_word_pos(word):
+def basic_words_pos(words):
     """
     Get POS-tag dataclass from a Word or Punc dataclass.
 
     inputs:
-    Word or Punc tuple of the form:
+    List of Words or Puncs tuples of the form:
         Word(<word>, <start time>, <end_time>) or
         Punc(<word>, <start time>, <end_time>)
-        e.g: Word(text="make", start_time=10.14, end_time=10.2)
+        e.g: [Word(text="make", start_time=10.14, end_time=10.2, weight=1.0),
+        Punc(text=".", start_time=10.25, end_time=10.25, weight=1.0)]
 
     outputs:
     Pos-tuple with added POS-tag of the form:
         Pos(<word>, <start time>, <end_time>)
-        e.g: Pos(text="make", start_time=10.14, end_time=10.2, tag="VBD")
+        e.g: [Pos(text="make", start_time=10.14, end_time=10.2, weight=1.0 tag="VBD"),
+        Pos(text=".", start_time=10.25, end_time=10.25, weight=1.0, tag=".")]]
     """
-    text = word.text.lower()
-    tag = nltk.pos_tag([text], tagset='universal')[0][1]
+    tagged_words = []
+    for word in words:
+        text = word.text.lower()
+        tag = nltk.pos_tag([text], tagset='universal')[0][1]
 
-    return Pos(word.text, word.start, word.end, tag=tag, weight=1.0)
+        tagged_words.append(Pos(word.text, word.start, word.end, word.weight, tag=tag))
 
-print(basic_word_pos(Word(text='had', start='322.65', end='322.75', weight=1.0)))
+    return tagged_words
+
+# print(basic_words_pos([Word(text='had', start='322.65', end='322.75', weight=1.0)]))
 
 
 def no_apos(tokenized_text):
@@ -102,5 +107,3 @@ def basic_pos(asrfile, lang='eng', sents=True, split_apos=False):
     return no_apos(nltk.pos_tag(nltk.word_tokenize(text)))
 
 pos = basic_pos('asr/sample01.asrOutput.json')
-
-tagged = basic_word_pos()
