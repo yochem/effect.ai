ff2f4343f0db61acfbd51e84c62f316681b7631f
Yochem van Rosmalen <yochem+git@icloud.com>
2020-06-15 12:32:13 +0200

    Add Google Style docstrings to asr.py

--- src/asr.py
+++ src/asr.py
@@ -1,8 +1,15 @@
-"""
-The ASR module.
+"""Automatic Speech Recognition module.
 
-Module for working with ASR (Automatic Speech Recognition) files formatted as
-JSON. It provides the ASR() class and the Word() and Punc() dataclasses.
+This Module is for working with ASR (Automatic Speech Recognition) files
+formatted as JSON. It provides the ASR class and the Word and Punc
+dataclasses.
+
+Example:
+    >>> import asr
+    >>> asr.ASR('/path/to/file.json').groups()
+    [Word(text='An', start=0, end=1, weight=0),
+     Word(text='example', start=1, end=2, weight=0),
+     Punc(text='.', start=2, end=2, weight=0)]
 """
 from dataclasses import dataclass
 import json
@@ -11,9 +18,16 @@ from typing import List, Union
 
 @dataclass
 class Word:
-    """
-    Basic word data. Containing the word the start time of the word, end time
-    of the word and the splitting weight.
+    """Python representation of a word from the ASR file.
+
+    Basic data of a spoken word.
+
+    Attributes:
+        text: The transcripted word.
+        start: Begin time of the word in seconds.
+        end: End time of the word in seconds.
+        weight: How good a split after this word would be. How higher the
+            value, how better the split would be.
     """
     text: str
     start: float
@@ -23,39 +37,64 @@ class Word:
 
 @dataclass
 class Punc(Word):
-    """
-    Basic punctuation data. Start and end time are kept for flexibility using
-    both Word() and Punc() together. This dataclass is used to simplify
-    recognising punctuation in the caption.
+    """Python representation of punctuation from the ASR file.
+
+    Basic data of punctuation.
+
+    Attributes:
+        text: The punctuation character.
+        start: End time of the word before the punctuation in seconds.
+        end: End time of the word before the punctuation in seconds.
+        weight: How good a split after this word would be. How higher the
+            value, how better the split would be.
     """
 
 
 class ASR:
-    """
+    """Automatic Speech Recognition class.
+
     This class helps working with ASR files. It provides an API for loading
     these files and converting it to varias datastructures.
+
+    Attributes:
+        data: all data from the ASR file loaded with the JSON module.
     """
 
     def __init__(self, filename: str):
-        """Load asr file with given filename. Returns JSON object."""
+        """Load asr file with given filename.
+
+        Args:
+            filename: A string of an ASR filename.
+        """
         with open(filename, 'r') as f:
             raw = f.read()
 
         self.data = json.loads(raw)
 
     def transcript(self) -> str:
-        """Return the transcript as one big string."""
+        """Return the transcript as one big string.
+
+        Returns:
+            A string containing the whole transcript.
+        """
         return self.data['results']['transcripts'][0]['transcript']
 
     def json(self) -> dict:
-        """Return the full JSON file as python dictionary."""
+        """Return the full JSON file as python dictionary.
+
+        Returns:
+            a dictionary containing the transcript and all words with times
+            and precision rates. For full overview of what this returns, see
+            the contents of ../asr/sample01.asrOutput.json.
+        """
         return self.data
 
     def groups(self) -> List[Union[Word, Punc]]:
-        """
-        Convert the ASR to the following format to a list of Word() and
-        Punc()'s. The Word() and Punc() are dataclasses containing the textual
-        representation and the start and end time.
+        """Convert the ASR to the following format to a list of Word and
+        Punc's.
+
+        Returns:
+            A list of Word and Punc dataclasses with weights initialised at 0.
         """
         cap = []
 
